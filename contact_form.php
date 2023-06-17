d9fdd0c079cd5bd78315c9796552cbd8089db159
manudelpino <manudelpino0@gmail.com>
2023-06-16 23:58:13 -0300

    added contact form functionality

---
+++ contact_form.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["user_name"];
+    $email = $_POST["user_email"];
+    $message = $_POST["user_message"];
+
+    $to = "tucorreo@example.com";
+    $subject = "Nuevo mensaje del formulario de contacto";
+    $body = "Nombre: " . $name . "\n";
+    $body .= "Email: " . $email . "\n";
+    $body .= "Mensaje: " . $message;
+
+    if (mail($to, $subject, $body)) {
+        echo "¡Mensaje enviado con éxito!";
+    } else {
+        echo "Hubo un error al enviar el mensaje.";
+    }
+}
+?>
